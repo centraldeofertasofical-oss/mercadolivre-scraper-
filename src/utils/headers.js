d556c2b601cbf42cc808fcff7a1f301fd546b688
centraldeofertasofical-oss <centraldeofertas.ofical@gmail.com>
2026-04-02 15:35:43 -0300

    headers.js

--- src/utils/headers.js
+++ src/utils/headers.js
@@ -1,1 +1,12 @@
+import { settings } from '../config/settings.js';
 
+export function buildHeaders() {
+  return {
+    'User-Agent': settings.defaultUserAgent,
+    'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,*/*;q=0.8',
+    'Accept-Language': 'pt-BR,pt;q=0.9,en-US;q=0.8,en;q=0.7',
+    'Cache-Control': 'no-cache',
+    'Pragma': 'no-cache',
+    'Upgrade-Insecure-Requests': '1'
+  };
+}
